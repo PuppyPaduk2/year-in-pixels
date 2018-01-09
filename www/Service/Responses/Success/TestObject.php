e4da725f13988dedeb7ed0b18664c23c3f677925
puppypaduk2 <debraooclark@gmail.com>
2018-01-09 22:48:00 +0300

    Удалил ненужные файлы

--- www/Service/Responses/Success/TestObject.php
+++
@@ -1,9 +0,0 @@
-<?php
-   response("GET", "test={0,1}(.*)", function ($app, $ninjphp, $query, $route) {
-      $ninjphp->including(["data-base"]);
-
-      $result = Object::call($query->data(), $app->config, "Obj/");
-
-      print_r($result);
-   });
-?>
