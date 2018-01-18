c0c5ec5a7803ad5e0664018f837ea00d6b1b4297
puppypaduk2 <debraooclark@gmail.com>
2018-01-18 18:09:49 +0300

    Вынес код в классы. Настройка роутинга

--- www/Client/Pages/Years/Router.js
+++ www/Client/Pages/Years/Router.js
@@ -1,92 +1,24 @@
 define([
    'Pages/Years/Views/Year',
-   'Pages/Years/Views/Palette',
-   'Views/FloatArea/FloatArea',
-   'Pages/Years/Helpers',
-   'jade!Pages/Years/Templates/Palette',
-   'Core/Service',
-
-   'css!Pages/Years/Styles/Main',
-   'css!Pages/Years/Styles/Palette'
-], function (Year, Palette, FloatArea, Helpers, tPalette, service) {
+   'css!Pages/Years/Styles/Main'
+], function (Year) {
    'use strict';
 
-   var $body = $('body');
+   var Router = Backbone.Router.extend({
+      routes: {
+         // '/(.*)/': 'main'
+      },
+      initialize: function(options) {
+         this.route(/(.*)/, 'main');
+      },
+      main: function(path) {
+         console.log('main', arguments);
 
-   var palette = tPalette({
-      palette: window.palette,
-      style: Helpers.styleColorBlock
+         new Year({
+            el: $('body')
+         });
+      }
    });
 
-   var vPalette = new Palette({
-      el: $(palette)
-   });
-
-   var paletteButton = new Palette({
-      el: $(palette)
-   });
-
-   /**
-    * Экземпляр пелетки с цветами
-    */
-   var floatArea = new FloatArea({
-      $el: vPalette.$el,
-      $border: $('.content .table')
-   });
-
-   var paletteButtonFA = new FloatArea({
-      $el: paletteButton.$el,
-      $border: $body
-   });
-
-   var year = new Year({
-      el: $body
-   });
-
-   year.listenTo(vPalette, 'click', function(date, data) {
-      // Отправим данные на сервер
-      service.post('Days.Write', {
-         date: date,
-         status: data.status
-      }, {
-         success: function(result) {
-            this.colorDay(date, data.color);
-         }.bind(this)
-      });
-   });
-
-   // ОБработчик клика по цветному блоку
-   year.on('clickBlockColor', function(e) {
-      var $target = $(e.target);
-
-      e.stopPropagation();
-
-      floatArea.show($target);
-
-      vPalette.date = $target.data().date;
-
-      paletteButtonFA.hide();
-
-      this.setBlur(true);
-   });
-
-   // Обработчик клика по всей области
-   year.on('click', function(e) {
-      floatArea.hide();
-      paletteButtonFA.hide();
-      this.setBlur(false);
-   });
-
-   // Клик по кнопке с палеткой
-   year.$('.button[name="palette"]').click(function(e) {
-      var $target = $(e.target);
-
-      e.stopPropagation();
-
-      paletteButtonFA.show($target);
-   });
-
-   if (window.palette) {
-      delete window.palette;
-   }
+   new Router();
 });
--- www/Client/Pages/Years/Views/Year.js
+++ www/Client/Pages/Years/Views/Year.js
@@ -1,4 +1,9 @@
-define(['Pages/Years/Helpers'], function(Helpers) {
+define([
+   'Pages/Years/Views/Palette',
+   'Views/FloatArea/FloatArea',
+   'Core/Service',
+   'Pages/Years/Helpers'
+], function(Palette, FloatArea, Service, Helpers) {
    'use strict';
 
    return Backbone.View.extend({
@@ -6,13 +11,52 @@ define(['Pages/Years/Helpers'], function(Helpers) {
        * @config {Object}
        */
       events: {
-         'click .table .block-color': function(e) {
-            this.trigger('clickBlockColor', e);
-         },
+         // Клик в любую область представления
+         'click': '_click',
 
-         'click': function(e) {
-            this.trigger('click');
-         }
+         // Клик по блоку дня
+         'click .table .block-color': '_clickBlockColor',
+
+         // Клик по кнопке с палеткой
+         'click .button[name="palette"]': '_clickButtonPalette'
+      },
+
+      initialize: function () {
+         // Палетка для блоков дней
+         this.daysPalette = new Palette();
+         this.daysPaletteFloatArea = new FloatArea({
+            $el: this.daysPalette.$el,
+            $border: $('.content .table'),
+            offset: {
+               top: -6,
+               left: -7
+            }
+         });
+
+         // Будем слушать события клика по элементу палетки
+         this.listenTo(this.daysPalette, 'click', function(data) {
+            var date = this.daysPaletteFloatArea.date;
+
+            // Отправим данные на сервер
+            Service.post('Days.Write', {
+               date: date,
+               status: data.status
+            }, {
+               success: function(result) {
+                  this.colorDay(date, data.color);
+               }.bind(this)
+            });
+         });
+
+         // Палетка для кнопкии "палетка"
+         this.buttonPaletteFloatArea = new FloatArea({
+            $el: new Palette().$el,
+            $border: $('body'),
+            offset: {
+               top: -5,
+               left: -5
+            }
+         });
       },
 
       /**
@@ -31,6 +75,46 @@ define(['Pages/Years/Helpers'], function(Helpers) {
       colorDay: function(date, color) {
          this.$('.content .table .block-color[data-date=' + date + ']')
             .attr('style', Helpers.styleColorBlock(color));
+      },
+
+      /**
+       * Обработчик клика в любую область представления
+       */
+      _click: function() {
+         this.daysPaletteFloatArea.hide();
+         this.buttonPaletteFloatArea.hide();
+         this.setBlur(false);
+      },
+
+      /**
+       * Обработчик клика по блоку дня
+       */
+      _clickBlockColor: function(e) {
+         var $target = $(e.target);
+
+         e.stopPropagation();
+
+         this.daysPaletteFloatArea.show($target);
+         this.daysPaletteFloatArea.date = $target.data().date;
+
+         // Скроем палетку для кнопки
+         this.buttonPaletteFloatArea.hide();
+
+         // Размытие контента
+         this.setBlur(true);
+      },
+
+      /**
+       * Обработчик клика по кнопке с палеткой
+       */
+      _clickButtonPalette: function(e) {
+         var $target = $(e.target);
+
+         e.stopPropagation();
+
+         this.daysPaletteFloatArea.hide();
+         this.setBlur(false);
+         this.buttonPaletteFloatArea.show($target);
       }
    });
 });
